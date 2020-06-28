8649f06c9a2f0a8eea3f280b0da4d5ece1ad8401
Dina <diana.liubarets@hotmail.com>
2020-06-28 19:45:59 -0400

    commit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:DiLiubarets/Vue-Project-aboutCrypto.git master:gh-pages
+git push -f git@github.com:DiLiubarets/LSD-inVueJS.git master:gh-pages
 
 cd -
